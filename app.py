8f7262b1e0ffae4acdb0f064ed7c4c3e36bde5e2
xatckerx-hash <xatckerx@gmail.com>
2025-12-14 17:35:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,4 +14,5 @@ def handle_message(data):
     emit("message", data, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
