808cb218ee9a81c71074b4a2f08a2f83959cb649
xatckerx-hash <xatckerx@gmail.com>
2025-12-14 18:18:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
+app.config["SECRET_KEY"] = "secret"
+
+socketio = SocketIO(
+    app,
+    async_mode="eventlet",
+    cors_allowed_origins="*",
+    logger=False,
+    engineio_logger=False
+)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("connect")
+def connect():
+    print("Client connected")
+
+@socketio.on("disconnect")
+def disconnect():
+    print("Client disconnected")
 
 @socketio.on("message")
 def handle_message(data):
